60e277c6c11461070f2e91939f9e9a116397596a
2471186676 <alex145561625@gmail.com>
2021-10-10 21:12:12 +0800

    add ship, remove ship function

--- src/BS_script/factoryConstruct.js
+++ src/BS_script/factoryConstruct.js
@@ -1,14 +1,17 @@
-const shipPart = (x,y) => {
+const shipPart = (x, y) => {
 	let hit = false;
 	let reveal = false;
-	return { hit, reveal, x, y };
-};
+	x = Number(x);
+	y = Number(y);
 
+	let asString = Number(x) + "-" + Number(y);
+	return { hit, reveal, x, y, asString };
+};
 
 const ship = (size, direction, x, y) => {
 	let body = [];
 	for (let i = 0; i < size; i++) {
-		body.push(shipPart(x[i],y[i]));
+		body.push(shipPart(x[i], y[i], ship));
 	}
 
 	const getHit = (loc) => {
@@ -26,16 +29,15 @@ const ship = (size, direction, x, y) => {
 	return { body, direction, getHealth, getHit, x, y };
 };
 
-
 const gameBoard = (size, belong) => {
 	let board = new Array(size);
 	let ships = [];
 
-	const createBoard = () => {
-		for (var i = 0; i < board.length; i++) {
-			board[i] = new Array(size);
-		}
+	for (var i = 0; i < board.length; i++) {
+		board[i] = new Array(size);
+	}
 
+	const resetBoard = () => {
 		for (let x = 0; x < size; x++) {
 			for (let y = 0; y < size; y++) {
 				board[x][y] = undefined;
@@ -44,16 +46,51 @@ const gameBoard = (size, belong) => {
 	};
 
 	const addShip = (size, direction, x, y) => {
-		let newShip = ship(size, direction, x, y);
-		ships.push(newShip);
-	}
+		// x & y is an array and length == size
+		let isEmpty = true;
+		for (let i = 0; i < size; i++) {
+			if (board[x[i]][y[i]] != undefined) {
+				isEmpty = false;
+				i = size + 2;
+			}
+		}
+		if (isEmpty) {
+			let newShip = ship(size, direction, x, y);
+			ships.push(newShip);
+		} else {
+			alert("not Empty");
+		}
 
-	return { board, belong, createBoard, size, addShip, ships};
+		resetBoard();
+		updateBoard();
+	};
+
+	const updateBoard = () => {
+		ships.forEach((ship) => {
+			ship.body.forEach((part) => {
+				// console.log(ship);
+				let x = part.x;
+				let y = part.y;
+				board[x][y] = part;
+			});
+		});
+	};
+
+	const removeShip = (location) => {
+		// sort through every ships part location
+		for (let curShip = 0; curShip < ships.length; curShip++) {
+			ships[curShip].body.forEach((part) => {
+				if (part.asString == location) {
+					ships.splice(curShip, 1);
+				}
+			});
+		}
+
+		resetBoard();
+		updateBoard();
+	};
+
+	return { board, belong, size, addShip, resetBoard, removeShip };
 };
-
-
-
-
-
 
 export { gameBoard, ship };
--- src/BS_script/grid.js
+++ src/BS_script/grid.js
@@ -1,1 +1,88 @@
 import { gameBoard, ship, board } from "./factoryConstruct";
+
+const createPlayerBoard = (gameBoard) => {
+	let board = document.createElement("div");
+	board.className = "board";
+	board.id = gameBoard.belong;
+	let boardSize = gameBoard.size;
+
+	for (let y = 0; y < boardSize; y++) {
+		for (let x = 0; x < boardSize; x++) {
+			let block = document.createElement("div");
+			block.className = "block";
+			block.id = x + "-" + y;
+
+			block.addEventListener("click", (event) => {
+				if (!getRemove()) {
+					let shipSize = getSize();
+
+					let locX = getDir() ? inAdditional(x, shipSize) : inConsistence(x, shipSize);
+					let locY = getDir() ? inConsistence(y, shipSize) : inAdditional(y, shipSize);
+				
+					gameBoard.addShip(shipSize, getDir, locX, locY);
+				}
+
+				if (getRemove()) {
+					let getLocation = event.target.id;
+
+					gameBoard.removeShip(getLocation);
+				}
+
+				updateGameBoard(gameBoard);
+			});
+
+			board.appendChild(block);
+		}
+	}
+
+	return board;
+};
+
+const updateGameBoard = (gameBoard) => {
+	const boardUI = document.getElementById(gameBoard.belong);
+
+	let boardSize = gameBoard.size;
+	for (let x = 0; x < boardSize; x++) {
+		for (let y = 0; y < boardSize; y++) {
+			let block = boardUI.querySelector("[id='" + x + "-" + y + "']");
+			if (gameBoard.board[x][y] != null) {
+				block.style.backgroundColor = "red";
+			} else {
+				block.style.backgroundColor = "";
+			}
+		}
+	}
+};
+
+const inAdditional = (num, size) => {
+	let array = [];
+	for (let i = 0; i < size; i++) {
+		array.push(Number(num + i));
+	}
+	return array;
+};
+
+const inConsistence = (num, size) => {
+	let array = [];
+	for (let i = 0; i < size; i++) {
+		array.push(num);
+	}
+	return array;
+};
+
+const getDir = () => {
+	return document.getElementById("dir").checked;
+};
+
+const getSize = () => {
+	return document.querySelector("input[name='type']:checked").value;
+};
+
+const getRemove = () => {
+	return document.getElementById("remove").checked;
+};
+
+let gameBoard1 = gameBoard(10, "as");
+let gameBoard2 = gameBoard(10, "ss");
+document.body.appendChild(createPlayerBoard(gameBoard1));
+document.body.appendChild(createPlayerBoard(gameBoard2));
